eef07b5753671a11ad4571beed94ada60bf71d46
Abdallah Bari <abdellah01win@gmail.com>
2021-12-03 16:18:30 +0100

    Get user's date and appen it to the each tour in DB

--- index.js
+++ index.js
@@ -5,11 +5,29 @@ const dataBase = require('./tours.json')
 //setup EJS 
 app.set('view engine', 'ejs');
 app.get('/', (req, res)=>{
-    res.render('index');
+    res.render('index', {...dataBase});
 })
 
 app.use(express.static('Public'));
+// Async function to get the user's Date
+const getDate = async ()=>{
+    try{
+        let currentDate = new Date();
+        console.log(`${currentDate.getDay()}/${currentDate.getMonth() + 1 }/${currentDate.getFullYear()}`);
+        //for each tour in DB, get user date and append it to the tour's info
 
+        for (const tour in dataBase) {
+            let tourDate = `${currentDate.getDay()}/${currentDate.getMonth() + 1 }/${currentDate.getFullYear()}`
+            Object.assign(dataBase[tour], {"date" : tourDate} )
+            console.log(dataBase[tour]['price']);
+        }
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+getDate();
+// Async function to generate fuure dates for the tours in the DB
 app.listen(3000, ()=>{
     console.log('listening on port 3000')
 })
