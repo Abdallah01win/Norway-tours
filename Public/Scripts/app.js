d8673ce29af2bbde0c772bcfe2393522d6a0bea7
Abdallah Bari <abdellah01win@gmail.com>
2021-12-09 18:08:36 +0100

    animate section -c forms

--- Public/Scripts/app.js
+++ Public/Scripts/app.js
@@ -6,12 +6,27 @@ const logInForm = document.getElementById('log-in-form');
 
 logInBTN.addEventListener('click', (e)=>{
     e.preventDefault();
-    signUpForm.classList.add('form-hidden');
-    logInForm.classList.remove('form-hidden');
+    signUpForm.classList.remove('animate-up');
+    logInForm.classList.remove('animate-out');
+
+    signUpForm.classList.add('animate-out');
+    setTimeout(() => {    
+        signUpForm.classList.add('form-hidden');
+        logInForm.classList.remove('form-hidden');
+    }, 300);
+    logInForm.classList.add('animate-up');
 })
+
 signUpBTN.addEventListener('click', (e)=>{
     e.preventDefault();
-    signUpForm.classList.remove('form-hidden');
-    logInForm.classList.add('form-hidden');
+    signUpForm.classList.remove('animate-out');
+    logInForm.classList.remove('animate-up');
+
+    logInForm.classList.add('animate-out');
+    setTimeout(() => {  
+        logInForm.classList.add('form-hidden');
+        signUpForm.classList.remove('form-hidden');
+    }, 300);
+    signUpForm.classList.add('animate-up');
 })
 
