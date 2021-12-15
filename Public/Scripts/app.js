09010d51e974941b237a8ef14a7d464e24c7e1a1
Abdallah Bari <abdellah01win@gmail.com>
2021-12-15 14:53:11 +0100

    add review section to tour pages

--- Public/Scripts/app.js
+++ Public/Scripts/app.js
@@ -3,9 +3,9 @@ const signUpBTN = document.querySelector('#sec-sign-up-btn');
 const signUpForm = document.getElementById('sign-up-form');
 const logInForm = document.getElementById('log-in-form');
 const detailsBtn = document.querySelectorAll('.details-btn');
-const headr = document.getElementsByClassName('header');
+const headr = document.getElementsByTagName('header');
 //const dataBase = require('./tours.json')
-import * as dataBase from 'F:\Projects\Norway Tours -v 2/tours.json';
+//import * as dataBase from 'F:\Projects\Norway Tours -v 2/tours.json';
 logInBTN.addEventListener('click', (e) => {
     e.preventDefault();
     signUpForm.classList.remove('animate-up');
@@ -42,15 +42,15 @@ detailsBtn.forEach(btn => {
         const tourThree = tourTow.nextElementSibling;
         if (card === tourOne) {
             btn.setAttribute("href", "/light-chase")
-            Header.style.backgroundImage = `${dataBase[0]["imgURL"]}`;
+            //header.style.backgroundImage = `${dataBase[0]["imgURL"]}`;
         }
         else if (card === tourTow) {
             btn.setAttribute("href", "/mountain-explorer")
-            Header.style.backgroundImage = "url(../Assets/Hero-img.jpg)";
+            //header.style.backgroundImage = "url(../Assets/Hero-img.jpg)";
         }
         else if (card === tourThree) {
             btn.setAttribute("href", "/country-side")
-            Header.style.backgroundImage = "url(../Assets/Hero-img.jpg)";
+            //header.style.backgroundImage = "url(../Assets/Hero-img.jpg)";
         }
     })
 });
