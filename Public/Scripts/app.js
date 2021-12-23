fbb040aa71f631b0af3568775c33173dd5d56413
Abdallah Bari <abdellah01win@gmail.com>
2021-12-23 15:21:41 +0100

    add responsive navigation for small screens and fix font bugs

--- Public/Scripts/app.js
+++ Public/Scripts/app.js
@@ -4,56 +4,72 @@ const signUpForm = document.getElementById('sign-up-form');
 const logInForm = document.getElementById('log-in-form');
 const detailsBtn = document.querySelectorAll('.details-btn');
 const headr = document.getElementsByTagName('header');
-//const dataBase = require('./tours.json')
-//import * as dataBase from 'F:\Projects\Norway Tours -v 2/tours.json';
-logInBTN.addEventListener('click', (e) => {
-    e.preventDefault();
-    signUpForm.classList.remove('animate-up');
-    logInForm.classList.remove('animate-out');
+const menue = document.getElementById('menue');
+const closeIcon = document.getElementById('close');
+const navList = document.getElementById('nav-list');
+const nav = document.getElementById('nav');
+const navButtons = document.querySelector('.buttons');
 
-    signUpForm.classList.add('animate-out');
-    setTimeout(() => {
-        signUpForm.classList.add('form-hidden');
-        logInForm.classList.remove('form-hidden');
-    }, 300);
-    logInForm.classList.add('animate-up');
-})
+if (logInBTN !== null) {
 
-signUpBTN.addEventListener('click', (e) => {
-    e.preventDefault();
-    signUpForm.classList.remove('animate-out');
-    logInForm.classList.remove('animate-up');
+    logInBTN.addEventListener('click', (e) => {
+        e.preventDefault();
+        signUpForm.classList.remove('animate-up');
+        logInForm.classList.remove('animate-out');
 
-    logInForm.classList.add('animate-out');
-    setTimeout(() => {
-        logInForm.classList.add('form-hidden');
-        signUpForm.classList.remove('form-hidden');
-    }, 300);
-    signUpForm.classList.add('animate-up');
-})
-detailsBtn.forEach(btn => {
-    btn.addEventListener('click', function (e) {
-        let index = undefined;
-        const container = this.parentElement.parentElement.parentElement;
-        const card = this.parentElement.parentElement;
-
-        const tourOne = container.firstElementChild;
-        const tourTow = tourOne.nextElementSibling;
-        const tourThree = tourTow.nextElementSibling;
-        if (card === tourOne) {
-            btn.setAttribute("href", "/light-chase")
-            //header.style.backgroundImage = `${dataBase[0]["imgURL"]}`;
-        }
-        else if (card === tourTow) {
-            btn.setAttribute("href", "/mountain-explorer")
-            //header.style.backgroundImage = "url(../Assets/Hero-img.jpg)";
-        }
-        else if (card === tourThree) {
-            btn.setAttribute("href", "/country-side")
-            //header.style.backgroundImage = "url(../Assets/Hero-img.jpg)";
-        }
+        signUpForm.classList.add('animate-out');
+        setTimeout(() => {
+            signUpForm.classList.add('form-hidden');
+            logInForm.classList.remove('form-hidden');
+        }, 300);
+        logInForm.classList.add('animate-up');
     })
+
+    signUpBTN.addEventListener('click', (e) => {
+        e.preventDefault();
+        signUpForm.classList.remove('animate-out');
+        logInForm.classList.remove('animate-up');
+
+        logInForm.classList.add('animate-out');
+        setTimeout(() => {
+            logInForm.classList.add('form-hidden');
+            signUpForm.classList.remove('form-hidden');
+        }, 300);
+        signUpForm.classList.add('animate-up');
+    })
+    detailsBtn.forEach(btn => {
+        btn.addEventListener('click', function (e) {
+            let index = undefined;
+            const container = this.parentElement.parentElement.parentElement;
+            const card = this.parentElement.parentElement;
+
+            const tourOne = container.firstElementChild;
+            const tourTow = tourOne.nextElementSibling;
+            const tourThree = tourTow.nextElementSibling;
+            if (card === tourOne) {
+                btn.setAttribute("href", "/light-chase")
+            }
+            else if (card === tourTow) {
+                btn.setAttribute("href", "/mountain-explorer")
+            }
+            else if (card === tourThree) {
+                btn.setAttribute("href", "/country-side")
+            }
+        })
+    });
+}
+menue.addEventListener('click', () => {
+    menue.classList.add('hide-icon');
+    closeIcon.classList.add('show-icon');
+    navList.classList.add('show-menue');
+    navButtons.classList.add('show-buttons');
+    //change the green logo to the white one by changing logo img src
 });
 
-
+closeIcon.addEventListener('click', () => {
+    menue.classList.remove('hide-icon');
+    closeIcon.classList.remove('show-icon');
+    navList.classList.remove('show-menue');
+    navButtons.classList.remove('show-buttons');
+})
 
