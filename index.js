81b7bd0fc4d0fe84f7c082660b1bb47f1fcc29d3
Abdallah Bari <abdellah01win@gmail.com>
2021-12-23 21:14:10 +0100

    change port to PORT

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ const getDate = async () => {
     }
 }
 getDate();
-app.listen(process.env.port || 3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('listening on port 3000');
 })
 
